67768421eaa102ff84bc7c42593344f80ad1d1a2
Tinotenda Gusha <st10298329@vcconnect.edu.za>
2024-08-25 16:22:34 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RetailWebApp_st10298329.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
