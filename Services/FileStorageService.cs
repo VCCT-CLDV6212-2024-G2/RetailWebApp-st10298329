eb31ffc910f77293a9f872d5747bb2fd12e91f2e
Tinotenda Gusha <st10298329@vcconnect.edu.za>
2024-08-25 19:23:49 +0200

    This commit adds a new class FileStorageService to the services folder. The class provides methods for uploading and downloading files from a specified directory.

---
+++ Services/FileStorageService.cs
@@ -0,0 +1,29 @@
+﻿namespace RetailWebApp_st10298329.Services
+{
+    public class FileStorageService
+    {
+        private readonly ShareClient _shareClient;
+
+        public FileStorageService(string storageConnectionString, string shareName)
+        {
+            var serviceClient = new ShareServiceClient(storageConnectionString);
+            _shareClient = serviceClient.GetShareClient(shareName);
+            _shareClient.CreateIfNotExists();
+        }
+
+        public async Task UploadFileAsync(string fileName, Stream content)
+        {
+            var directoryClient = _shareClient.GetRootDirectoryClient();
+            var fileClient = directoryClient.GetFileClient(fileName);
+            await fileClient.CreateAsync(content.Length);
+            await fileClient.UploadAsync(content);
+        }
+
+        public async Task<Stream> DownloadFileAsync(string fileName)
+        {
+            var fileClient = _shareClient.GetRootDirectoryClient().GetFileClient(fileName);
+            var downloadInfo = await fileClient.DownloadAsync();
+            return downloadInfo.Value.Content;
+        }
+    }
+}
