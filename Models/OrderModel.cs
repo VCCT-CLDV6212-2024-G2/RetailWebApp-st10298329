43019bb99ef1b2e9a3d67ace69640b64d1508ea1
Tinotenda Gusha <st10298329@vcconnect.edu.za>
2024-08-29 19:22:00 +0200

    This commit is for the newly created order class model. It represents an order in the system. It stores and retrieves data in Azure Table Storage

---
+++ Models/OrderModel.cs
@@ -0,0 +1,26 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace RetailWebApp_st10298329.Models
+{
+    public class OrderModel : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        public string ProductId { get; set; }
+        public string CustomerId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public OrderModel() { }
+
+        public OrderModel(string partitionKey, string rowKey)
+        {
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
+        }
+    }
+}
