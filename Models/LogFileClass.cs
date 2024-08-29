1add4cb045a19be2eb1a5c80268ac8d634560e18
Tinotenda Gusha <st10298329@vcconnect.edu.za>
2024-08-29 19:25:25 +0200

    Created LogFile class that represents a log file in the app. Contains properties and methods necessary for storing, retrieving, and managing log data within Azure File Storage.

---
+++ Models/LogFileClass.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Storage.Files.Shares;
+using Azure;
+using System;
+using System.IO;
+using Azure.Storage.Files.Shares.Models;
+
+namespace RetailWebApp_st10298329.Models
+{
+    public class LogFileClass
+    {
+        public string FileName { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public LogFileClass() { }
+
+        public LogFileClass(string fileName, string content)
+        {
+            FileName = fileName;
+            Content = content;
+            CreatedDate = DateTime.UtcNow;
+        }
+
+        public async Task UploadAsync(ShareClient shareClient)
+        {
+            var directoryClient = shareClient.GetRootDirectoryClient();
+            var fileClient = directoryClient.GetFileClient(FileName);
+            using (var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(Content)))
+            {
+                await fileClient.CreateAsync(stream.Length);
+                await fileClient.UploadRangeAsync(new HttpRange(0, stream.Length), stream);
+            }
+        }
+
+        public async Task<string> DownloadAsync(ShareClient shareClient)
+        {
+            var directoryClient = shareClient.GetRootDirectoryClient();
+            var fileClient = directoryClient.GetFileClient(FileName);
+            var downloadInfo = await fileClient.DownloadAsync();
+            using (var reader = new StreamReader(downloadInfo.Value.Content))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
